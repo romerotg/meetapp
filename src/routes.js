830b4083f9a77f27e7f8760af101482ca7293dd4
Romero Gonçalves <romerotg@gmail.com>
2019-07-03 19:00:17 -0300

    feature: adding support for uploading files

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
 
 routes.use(authMiddleware);
 
+// user routes
 routes.get('/users', UserController.index);
 routes.put('/users', UserController.update);
+
+// file upload routes
+routes.post('files', upload.single('file'), FileController.store);
 
 export default routes;
